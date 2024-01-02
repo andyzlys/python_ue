354d849f20260d5eedb8a63756d4f34be60cecbc
andyzlys <130483967+andyzlys@users.noreply.github.com>
2024-01-02 14:35:01 +0800

    Update set_a_new_ikrig.py

--- set_a_new_ikrig.py
+++ set_a_new_ikrig.py
@@ -20,16 +20,37 @@ else:
 
     # 创建IKRigDefinition资产
     ik_rig_definition_factory = unreal.IKRigDefinitionFactory()
-    ik_rig_definition_factory.set_editor_property('preview_skeletal_mesh', asset_to_check)  # 指定骨骼网格
+    ik_rig_definition_factory.set_editor_property('edit_after_new', True)
     ik_rig_definition = asset_tools.create_asset_with_dialog(ik_rig_asset_name, asset_path, unreal.IKRigDefinition, ik_rig_definition_factory)
+
+    # 保存对IKRigDefinition资产的更改
+    asset_full_path = f"{asset_path}/{ik_rig_asset_name}.{ik_rig_asset_name}"  # 构建资产的完整路径
+    saved = unreal.EditorAssetLibrary.save_asset(asset_full_path, only_if_is_dirty=True)
+    if saved:
+        print(f"Asset saved successfully: {asset_full_path}")
+    else:
+        print(f"Failed to save asset: {asset_full_path}")
+
 
     if ik_rig_definition:
         print(f"Created new IKRigDefinition asset: {asset_path}/{ik_rig_asset_name}")
 
         ik_rig_definition.set_editor_property('preview_skeletal_mesh', asset_to_check)
+
+        # asset_full_path = f"{asset_path}/{ik_rig_asset_name}.{ik_rig_asset_name}"  # 构建资产的完整路径
+        # saved = unreal.EditorAssetLibrary.save_asset(asset_full_path, only_if_is_dirty=True)
+        # if saved:
+        #     print(f"Asset saved successfully: {asset_full_path}")
+        # else:
+        #     print(f"Failed to save asset: {asset_full_path}")
+
+        ik_rig_definition.set_editor_property('draw_goals', True)
         ik_rig_controller = unreal.IKRigController.get_controller(ik_rig_definition)
-        ik_rig_controller.add_retarget_chain('spine', 'spine_01', 'spine_02', None)
+        ik_rig_controller.add_retarget_chain('spine', 'spine_01', 'spine_02', 'None')
 
 
     else:
         print("Failed to create IKRigDefinition asset.")
+
+
+        
